4f5bf28862c1d91bcfb844088e0059d5de72852a
balazsvig <balazs.vig@icellmobilsoft.hu>
2025-01-11 16:58:21 +0100

    fixed test

--- todo-app-simplified/src/app/common/todo-edit-modal/todo-edit-modal.component.spec.ts
+++ todo-app-simplified/src/app/common/todo-edit-modal/todo-edit-modal.component.spec.ts
@@ -1,23 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoEditModalComponent } from './todo-edit-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 describe('TodoEditModalComponent', () => {
-  let component: TodoEditModalComponent;
   let fixture: ComponentFixture<TodoEditModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoEditModalComponent]
-    })
-      .compileComponents();
+      imports: [BrowserAnimationsModule, MatDialogModule, TodoEditModalComponent],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: undefined},
+        {provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close'])}
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoEditModalComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture).toBeTruthy();
   });
 });
